2a6f076f42812f4a3f9a958e0a49dd6e1a892a6a
Lívia Lima <liviac.lima@hotmail.com>
2021-01-19 00:03:35 -0300

    feat(vehicle-search): added shared module and vehicle search configs

---
+++ src/app/shared/services/vehicle-list.service.ts
@@ -0,0 +1,50 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { map } from 'rxjs/operators';
+import { VehicleSearchParamsModel } from 'src/app/vehicle-search/models/parse-models/vehicle-search-params.model';
+
+import { BrandList } from '../../vehicle-search/models/interfaces/brand-list.interface';
+import { FilterHelper } from '../helper/filter.helper';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class VehicleListService {
+
+
+
+  constructor(
+    private http: HttpClient,
+  ) { }
+
+  getBrandList(vehicleType){
+    return this.http.get<BrandList[]>(`https://fipeapi.appspot.com/api/1/${vehicleType}/marcas.json`)
+  }
+
+  getModelList(params: VehicleSearchParamsModel) {
+    const url = `https://fipeapi.appspot.com/api/1/${params.vehicleType}/veiculos/${params.brandId}.json`;
+    return this.http.get(url).pipe(
+      map((values: []) => {
+        let modelList
+        params.model
+          ? modelList = FilterHelper.modelNameFilter(values, params.model) 
+          : modelList = values
+        return modelList
+      })
+    )
+  }
+
+  getVehiclesVariation(vehicleType: string, brandId: string, vehicleId: string) {
+    const url = 
+      `https://fipeapi.appspot.com/api/1/${vehicleType}/veiculo/${brandId}/${vehicleId}.json`;
+    return this.http.get(url)
+  }
+
+  getVehiclesPrice(vehicleType: string, brandId: string, vehicleId: string, yearId: string) {
+    const url = 
+      `https://fipeapi.appspot.com/api/1/${vehicleType}/veiculo/${brandId}/${vehicleId}/${yearId}.json`;
+    return this.http.get(url)
+
+  }
+
+}
